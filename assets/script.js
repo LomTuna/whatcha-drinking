c5a3d955cfdead2934c7065f893fb5452372eb00
Andrew S <andrewsales2004@gmail.com>
2022-08-29 20:10:40 -0500

    created initial YouTube API code

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+const finBtn = document.querySelector('.finishQuiz')
+finBtn.addEventListener('click', Video)
+
+function Video() {
+
+var tag = document.createElement('script');
+tag.id = 'iframe-demo';
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+    };
