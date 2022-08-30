766dcf7842063dc8e8159e1200b5f85bd278affd
Andrew S <andrewsales2004@gmail.com>
2022-08-30 17:58:03 -0500

    completed YouTube API code and display functions. Added comments in JS file

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ var tag = document.createElement('script');
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+      //  array with each video's info //
       var playerInfoList = [{id:'player1',height:'390',width:'640',videoId:'M7lc1UVf-VE'}, {id:'player2',height:'390',width:'640',videoId:'M7lc1UVf-VE'}, {id:'player3',height:'390',width:'640',videoId:'M7lc1UVf-VE'}]
 
       function onYouTubeIframeAPIReady() {
@@ -18,19 +19,21 @@ var tag = document.createElement('script');
           return;
         
         for(var i = 0; i < playerInfoList.length; i++) {
-          var makePlayer = makePlayer(playerInfoList[i]);
+          var curplayer = createPlayer(playerInfoList[i]);
         }
       }
-      function makePlayer(playerInfo) {
+      function createPlayer(playerInfo) {
         return new YT.Player(playerInfo.id, {
           height: playerInfo.height,
           width: playerInfo.width,
           videoId: playerInfo.videoId,
           playerVars: {
             'rel':0
-          },
+          }
         });
       }
+
+//  Functions to unhide one YouTube video and hide the rest depending on button clicked  //
 
 coldCoff.addEventListener('click', Video1)
 function Video1() {
@@ -38,14 +41,10 @@ function Video1() {
   var V2 = document.getElementById('player2');
   var V3 = document.getElementById('player3');
   var V4 = document.getElementById('player4');
-  var V5 = document.getElementById('player5');
-  var V6 = document.getElementById('player6');
-  V1.style.display = "block";
+  V1.style.display = "inline-block";
   V2.style.display = "none";
   V3.style.display = "none";
   V4.style.display = "none";
-  V5.style.display = "none";
-  V6.style.display = "none";
 }
 
 hotCoff.addEventListener('click', Video2)
@@ -54,12 +53,32 @@ function Video2() {
   var V2 = document.getElementById('player2');
   var V3 = document.getElementById('player3');
   var V4 = document.getElementById('player4');
-  var V5 = document.getElementById('player5');
-  var V6 = document.getElementById('player6');
   V1.style.display = "none";
-  V2.style.display = "block";
+  V2.style.display = "inline-block";
   V3.style.display = "none";
   V4.style.display = "none";
-  V5.style.display = "none";
-  V6.style.display = "none";
+}
+
+coldNon.addEventListener('click', Video3)
+function Video3() {
+  var V1 = document.getElementById('player1');
+  var V2 = document.getElementById('player2');
+  var V3 = document.getElementById('player3');
+  var V4 = document.getElementById('player4');
+  V1.style.display = "none";
+  V2.style.display = "none";
+  V3.style.display = "inline-block";
+  V4.style.display = "none";
+}
+
+hotNon.addEventListener('click', Video4)
+function Video4() {
+  var V1 = document.getElementById('player1');
+  var V2 = document.getElementById('player2');
+  var V3 = document.getElementById('player3');
+  var V4 = document.getElementById('player4');
+  V1.style.display = "none";
+  V2.style.display = "none";
+  V3.style.display = "none";
+  V4.style.display = "inline-block";
 }
